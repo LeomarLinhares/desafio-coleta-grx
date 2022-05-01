1c9c5faf2b85f27836cb5eae74459e63cc585091
Leomar Linhares <leomar_linhares@hotmail.com>
2022-05-01 10:55:26 -0300

    :sparkles: feat: create index.ts and start server

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    this.app.use(express.json());
+  }
+
+  public start(): void {
+    this.app.listen(5000, () => {
+      console.log('Server started on port 5000');
+    });
+  }
+}
+
+new App().start();
