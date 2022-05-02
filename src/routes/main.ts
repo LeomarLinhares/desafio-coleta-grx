ea0f590c0a5c2b4cfe5980667e78521dc86996b4
Leomar Linhares <leomar_linhares@hotmail.com>
2022-05-02 10:55:48 -0300

    :construction: feat: adds the question validation middleware in the POST method

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import mainController from "../database/controllers/mainController";
+import Validate from "../database/middlewares/validateAnswers";
 
 const main = Router();
 
-main.post("/", mainController.createAnswer);
+main
+  .post
+  (
+    "/",
+    Validate.ifAnswersExist,
+    mainController.createAnswer,
+  )
+  .get("/", mainController.getAnswers);
 
 export default main;
