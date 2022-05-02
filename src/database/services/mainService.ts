c6ea8fc0915616207788227ac5d8a58a82675d13
Leomar Linhares <leomar_linhares@hotmail.com>
2022-05-02 10:11:38 -0300

    :construction: feat: standardizes the return format of services as IMessage

--- src/database/services/mainService.ts
+++ src/database/services/mainService.ts
@@ -1,30 +1,43 @@
-import countHandler from '../../helpers/countHandler';
-import { IAnswer, IAnswerResponse, IAnswerWithCount } from '../../interfaces/IAnswer';
-import IMessage from '../../interfaces/IMessage';
-import MainModel from '../models/mainModel';
-import StatusCode from '../messages/statusCode';
+import countHandler from "../../helpers/countHandler";
+import {
+  IAnswer,
+  IAnswerResponse,
+  IAnswerWithCount,
+} from "../../interfaces/IAnswer";
+import IMessage from "../../interfaces/IMessage";
+import MainModel from "../models/mainModel";
+import StatusCode from "../messages/statusCode";
 
 class MainService {
-  public async create (answer: IAnswer): Promise<IAnswerResponse | IMessage> {
+  public async create(answer: IAnswer): Promise<IMessage> {
     try {
       const answerWithCount: IAnswerWithCount = countHandler(answer);
       const allAnswersBuffer: Buffer = await MainModel.read();
-      const allAnswers: IAnswerResponse[] = JSON.parse(allAnswersBuffer.toString());
-      const responseBuffer = await MainModel.create(JSON.stringify([...allAnswers, {
-        ...answerWithCount,
-        id: allAnswers.length + 1,
-      }]));
+      const allAnswers: IAnswerResponse[] = JSON.parse(
+        allAnswersBuffer.toString()
+      );
+      const responseBuffer = await MainModel.create(
+        JSON.stringify([
+          ...allAnswers,
+          {
+            ...answerWithCount,
+            id: allAnswers.length + 1,
+          },
+        ])
+      );
 
       const response = JSON.parse(responseBuffer.toString());
 
-      return response[response.length - 1];
-
+      return {
+        statusCode: StatusCode.CREATED,
+        message: response[response.length - 1],
+      };
     } catch (error) {
       console.warn(error);
       return {
         statusCode: StatusCode.INTERNAL_SERVER_ERROR,
-        message: 'Erro ao acessar arquivo de dados'
-      }
+        message: "Erro ao acessar arquivo de dados",
+      };
     }
   }
 }
