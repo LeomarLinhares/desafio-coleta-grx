01d7aa126cc4d2809fbdf97f10f43ec1a64a34e7
Leomar Linhares <leomar_linhares@hotmail.com>
2022-05-02 10:15:26 -0300

    :construction: feat: standardizes sending controller response

--- src/database/controllers/mainController.ts
+++ src/database/controllers/mainController.ts
@@ -3,8 +3,8 @@ import { Request, Response } from 'express';
 
 class MainController {
   public async createAnswer (req: Request, res: Response): Promise<void> {
-    const response = await MainService.createAnswer(req.body);
-    res.status(200).send(response);
+    const response = await MainService.create(req.body);
+    res.status(response.statusCode).send(response.message);
   }
 }
 
