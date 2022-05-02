dc4639d1c7032f54076037b3536554fdac358e83
Leomar Linhares <leomar_linhares@hotmail.com>
2022-05-02 09:45:54 -0300

    :construction: feat: defines the business rules in the services layer

--- src/database/services/mainService.ts
+++ src/database/services/mainService.ts
@@ -1,9 +1,27 @@
+import countHandler from '../../helpers/countHandler';
+import { IAnswer, IAnswerResponse, IAnswerWithCount } from '../../interfaces/IAnswer';
+import IMessage from '../../interfaces/IMessage';
 import MainModel from '../models/mainModel';
 
 class MainService {
-  public async create (answer): Promise<MainModel> {
-    const response = await MainModel.create(answer);
-    return response;
+  public async create (answer: IAnswer): Promise<IAnswerResponse | IMessage> {
+    try {
+      const answerWithCount: IAnswerWithCount = countHandler(answer);
+      const allAnswersBuffer: Buffer = await MainModel.read();
+      const allAnswers: IAnswerResponse[] = JSON.parse(allAnswersBuffer.toString());
+      const responseBuffer = await MainModel.create(JSON.stringify([...allAnswers, {
+        ...answerWithCount,
+        id: allAnswers.length + 1,
+      }]));
+      
+      const response = JSON.parse(responseBuffer.toString());
+
+      return response[response.length - 1];
+
+    } catch (error) {
+      console.warn(error);
+      return { message: 'Erro ao acessar arquivo de dados' }
+    }
   }
 }
 
